e9794f5bbea4f32388ac1bf20e6e9043dd1bd450
Hyung Lee and Skye Nguyen <skye@>
2018-08-07 13:16:01 -0700

    delete redundant code

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,8 @@ $(document).ready(function() {
   $('div.term').attr('id', function(n) {
     return 't' + (n+1);
   });
-  // $('p.term').attr('id', function(m) {
-  //   return 'p' + (m+1);
-  // });
 
   $(".term h3").click(function() {
-    // var id = $(this).attr('id');
     var id = $(this).parent().attr("id");
     var destination = "#" + id + " p";
     alert(destination);
